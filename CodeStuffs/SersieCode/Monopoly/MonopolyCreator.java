7e8af80d28a4b6ec985b2016f66188ff8c979b67
Sarsie <serenanh@gmail.com>
2016-11-04 16:36:32 -0700

    Added Serena's Monopoly Files

---
+++ CodeStuffs/SersieCode/Monopoly/MonopolyCreator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class MonopolyCreator
+{
+	public static void main (String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Welcome to Monopoly.");
+		Board board = new Board();
+		Die die = new Die(0,0);
+		boolean over = false; 
+
+		while(over == false)
+		{
+			
+		}
+	}
+	/* takes care of creating all the other classes
+	manages actions:
+		-asks user for input
+	*/
+}
