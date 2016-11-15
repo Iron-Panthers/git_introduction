bc8aba6c955d1a9ea8ee061d59e787eaf943467e
Isabel Dominik <icd107@live.com>
2016-11-14 16:25:51 -0800

    Fixed dice so that player 1 and player 2 dont always have the same roll value

--- Monopoly/Monopoly.java
+++ Monopoly/Monopoly.java
@@ -4,6 +4,7 @@ public class Monopoly
 	public static void main(String[] args)
 	{
 		Die d1 = new Die();
+		Die d2 = new Die();
 		Board b1 = new Board();
 		Player p1 = new Player(1, 0, 1000);
 		Player p2 = new Player(2, 0, 1000);
@@ -11,9 +12,10 @@ public class Monopoly
 		boolean playing = true;
 		while(playing)
 		{
-			int spaces = d1.rollValue();
-			System.out.println("Player 1 moved " + spaces + " steps");
-			p1.movePlayer(spaces, b1, d1);
+			int spaces1 = d1.rollValue();
+			int spaces2 = d2.rollValue();
+			System.out.println("Player 1 moved " + spaces1 + " steps");
+			p1.movePlayer(spaces1, b1, d1);
 			System.out.println("You have " + p1.getMoney() + " dollars");
 			System.out.println("You are on " + b1.properties[p1.getPosition()].name);
 			System.out.println("Price: " + b1.properties[p1.getPosition()].price);
@@ -41,8 +43,14 @@ public class Monopoly
 					System.out.println("Baneling exploded. It is now free to buy.");
 				}
 			}
-			System.out.println("Player 2 moved " + spaces + " steps");
-			p2.movePlayer(spaces, b1, d1);
+			System.out.println("Player 1 now has " + p1.getMoney() + " dollars");
+			if(p1.getMoney() < 1)
+			{
+				System.out.println("Player 2 wins!");
+				playing = false;
+			}
+			System.out.println("Player 2 moved " + spaces2 + " steps");
+			p2.movePlayer(spaces2, b1, d2);
 			System.out.println("You have " + p2.getMoney() + " dollars");
 			System.out.println("You are on " + b1.properties[p2.getPosition()].name);
 			System.out.println("Price: " + b1.properties[p2.getPosition()].price);
@@ -70,8 +78,10 @@ public class Monopoly
 					System.out.println("Baneling exploded. It is now free to buy.");
 				}
 			}
-			if(p1.getMoney() < 1|| p2.getMoney() < 1)
+			System.out.println("Player 2 now has " + p2.getMoney() + " dollars");
+			if(p2.getMoney() < 1)
 			{
+				System.out.println("Player 1 wins!");
 				playing = false;
 			}
 		}
