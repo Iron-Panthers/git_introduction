8cd49902d2dddbca2a264b072e7fcf780fe305cc
Alex Mak <alexanderajm@gmail.com>
2016-11-15 18:59:32 -0800

    Renamed WarCardGame.java to Main.java

---
+++ CodeStuffs/Alex/WarCardGame/src/Main.java
@@ -0,0 +1,5 @@
+/**
+ * Created by alex on 11/15/16.
+ */
+public class Main {
+}
--- CodeStuffs/Alex/WarCardGame/src/WarCardGame.java
+++
@@ -1,6 +0,0 @@
-public class WarCardGame {
-    public static void main(String[] args) {
-        Deck deck = new Deck();
-
-    }
-}
